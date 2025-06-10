6e25d03ecc3953f6d4ea7c7cdc60eae60235b632
ndcuong2k3 <21a100100051@students.hou.edu.vn>
2025-06-10 08:57:04 +0700

    Handle logic for Sach Form

--- Sach.Designer.cs
+++ Sach.Designer.cs
@@ -28,12 +28,328 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Sach";
+            groupBox1 = new GroupBox();
+            cbbNXB = new ComboBox();
+            label4 = new Label();
+            txtGia = new TextBox();
+            txtTheloai = new TextBox();
+            txtTacgia = new TextBox();
+            txtNamxuatban = new TextBox();
+            txtSoluong = new TextBox();
+            txtTensach = new TextBox();
+            txtMasach = new TextBox();
+            label9 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            dgdSach = new DataGridView();
+            txtTimkiem = new TextBox();
+            btnThem = new Button();
+            btnSua = new Button();
+            btnXoa = new Button();
+            btnLammoi = new Button();
+            btnTimkiem = new Button();
+            button6 = new Button();
+            btnLuu = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgdSach).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(cbbNXB);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(txtGia);
+            groupBox1.Controls.Add(txtTheloai);
+            groupBox1.Controls.Add(txtTacgia);
+            groupBox1.Controls.Add(txtNamxuatban);
+            groupBox1.Controls.Add(txtSoluong);
+            groupBox1.Controls.Add(txtTensach);
+            groupBox1.Controls.Add(txtMasach);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 19);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(332, 324);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Thông tin sách";
+            // 
+            // cbbNXB
+            // 
+            cbbNXB.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbbNXB.FormattingEnabled = true;
+            cbbNXB.Location = new Point(128, 275);
+            cbbNXB.Name = "cbbNXB";
+            cbbNXB.Size = new Size(198, 28);
+            cbbNXB.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(11, 278);
+            label4.Name = "label4";
+            label4.Size = new Size(97, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Nhà xuất bản";
+            // 
+            // txtGia
+            // 
+            txtGia.Location = new Point(128, 242);
+            txtGia.Name = "txtGia";
+            txtGia.Size = new Size(198, 27);
+            txtGia.TabIndex = 15;
+            // 
+            // txtTheloai
+            // 
+            txtTheloai.Location = new Point(128, 209);
+            txtTheloai.Name = "txtTheloai";
+            txtTheloai.Size = new Size(198, 27);
+            txtTheloai.TabIndex = 14;
+            // 
+            // txtTacgia
+            // 
+            txtTacgia.Location = new Point(128, 176);
+            txtTacgia.Name = "txtTacgia";
+            txtTacgia.Size = new Size(198, 27);
+            txtTacgia.TabIndex = 13;
+            // 
+            // txtNamxuatban
+            // 
+            txtNamxuatban.Location = new Point(128, 143);
+            txtNamxuatban.Name = "txtNamxuatban";
+            txtNamxuatban.Size = new Size(198, 27);
+            txtNamxuatban.TabIndex = 12;
+            // 
+            // txtSoluong
+            // 
+            txtSoluong.Location = new Point(128, 110);
+            txtSoluong.Name = "txtSoluong";
+            txtSoluong.Size = new Size(198, 27);
+            txtSoluong.TabIndex = 11;
+            // 
+            // txtTensach
+            // 
+            txtTensach.Location = new Point(128, 74);
+            txtTensach.Name = "txtTensach";
+            txtTensach.Size = new Size(198, 27);
+            txtTensach.TabIndex = 10;
+            // 
+            // txtMasach
+            // 
+            txtMasach.Location = new Point(128, 39);
+            txtMasach.Name = "txtMasach";
+            txtMasach.Size = new Size(198, 27);
+            txtMasach.TabIndex = 9;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(11, 143);
+            label9.Name = "label9";
+            label9.Size = new Size(102, 20);
+            label9.TabIndex = 8;
+            label9.Text = "Năm xuất bản";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(13, 247);
+            label7.Name = "label7";
+            label7.Size = new Size(31, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Giá";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(11, 212);
+            label6.Name = "label6";
+            label6.Size = new Size(62, 20);
+            label6.TabIndex = 5;
+            label6.Text = "Thể loại";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(11, 174);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Tác giả";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(11, 110);
+            label3.Name = "label3";
+            label3.Size = new Size(97, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Số lượng kho";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 74);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Tên sách";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(11, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Mã sách";
+            // 
+            // dgdSach
+            // 
+            dgdSach.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgdSach.Location = new Point(356, 67);
+            dgdSach.Name = "dgdSach";
+            dgdSach.RowHeadersWidth = 51;
+            dgdSach.Size = new Size(432, 323);
+            dgdSach.TabIndex = 1;
+            dgdSach.CellClick += dgdSach_CellClick;
+            // 
+            // txtTimkiem
+            // 
+            txtTimkiem.Location = new Point(356, 33);
+            txtTimkiem.Name = "txtTimkiem";
+            txtTimkiem.Size = new Size(322, 27);
+            txtTimkiem.TabIndex = 17;
+            // 
+            // btnThem
+            // 
+            btnThem.Location = new Point(12, 361);
+            btnThem.Name = "btnThem";
+            btnThem.Size = new Size(94, 29);
+            btnThem.TabIndex = 18;
+            btnThem.Text = "Thêm";
+            btnThem.UseVisualStyleBackColor = true;
+            btnThem.Click += btnThem_Click;
+            // 
+            // btnSua
+            // 
+            btnSua.Location = new Point(129, 361);
+            btnSua.Name = "btnSua";
+            btnSua.Size = new Size(94, 29);
+            btnSua.TabIndex = 19;
+            btnSua.Text = "Sửa";
+            btnSua.UseVisualStyleBackColor = true;
+            btnSua.Click += btnSua_Click;
+            // 
+            // btnXoa
+            // 
+            btnXoa.Location = new Point(244, 361);
+            btnXoa.Name = "btnXoa";
+            btnXoa.Size = new Size(94, 29);
+            btnXoa.TabIndex = 20;
+            btnXoa.Text = "Xóa";
+            btnXoa.UseVisualStyleBackColor = true;
+            btnXoa.Click += btnXoa_Click;
+            // 
+            // btnLammoi
+            // 
+            btnLammoi.Location = new Point(23, 396);
+            btnLammoi.Name = "btnLammoi";
+            btnLammoi.Size = new Size(315, 29);
+            btnLammoi.TabIndex = 21;
+            btnLammoi.Text = "Làm mới";
+            btnLammoi.UseVisualStyleBackColor = true;
+            btnLammoi.Click += btnLammoi_Click;
+            // 
+            // btnTimkiem
+            // 
+            btnTimkiem.Location = new Point(694, 33);
+            btnTimkiem.Name = "btnTimkiem";
+            btnTimkiem.Size = new Size(94, 29);
+            btnTimkiem.TabIndex = 22;
+            btnTimkiem.Text = "Tìm kiếm";
+            btnTimkiem.UseVisualStyleBackColor = true;
+            btnTimkiem.Click += btnTimkiem_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(645, 396);
+            button6.Name = "button6";
+            button6.Size = new Size(143, 29);
+            button6.TabIndex = 23;
+            button6.Text = "Về Trang chủ";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // btnLuu
+            // 
+            btnLuu.Location = new Point(356, 396);
+            btnLuu.Name = "btnLuu";
+            btnLuu.Size = new Size(143, 29);
+            btnLuu.TabIndex = 24;
+            btnLuu.Text = "Lưu";
+            btnLuu.UseVisualStyleBackColor = true;
+            btnLuu.Click += btnLuu_Click;
+            // 
+            // Sach
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLuu);
+            Controls.Add(button6);
+            Controls.Add(btnTimkiem);
+            Controls.Add(btnLammoi);
+            Controls.Add(btnXoa);
+            Controls.Add(btnSua);
+            Controls.Add(btnThem);
+            Controls.Add(txtTimkiem);
+            Controls.Add(dgdSach);
+            Controls.Add(groupBox1);
+            Name = "Sach";
+            Text = "Sach";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgdSach).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private GroupBox groupBox1;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private ComboBox cbbNXB;
+        private TextBox txtGia;
+        private TextBox txtTheloai;
+        private TextBox txtTacgia;
+        private TextBox txtNamxuatban;
+        private TextBox txtSoluong;
+        private TextBox txtTensach;
+        private TextBox txtMasach;
+        private Label label9;
+        private DataGridView dgdSach;
+        private TextBox txtTimkiem;
+        private Button btnThem;
+        private Button btnSua;
+        private Button btnXoa;
+        private Button btnLammoi;
+        private Button btnTimkiem;
+        private Button button6;
+        private Button btnLuu;
     }
 }
--- Sach.cs
+++ Sach.cs
@@ -1,20 +1,239 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using Microsoft.Data.SqlClient;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace BookStoreManagement
 {
     public partial class Sach : Form
     {
+        DBHelper dBHelper = new DBHelper();
         public Sach()
         {
             InitializeComponent();
+            LoadData();
+
         }
+
+        public void LoadData()
+        {
+            string query = "Select * from vvSach";
+            DataTable dataTable = new DataTable();
+            dataTable = dBHelper.ExecuteQuery(query);
+            dBHelper.FillDataGridView(dgdSach, dataTable);
+
+            string cbquery = "SELECT sMaNXB, sTenNXB FROM tblNXB";
+            dBHelper.FillComboBox(cbbNXB, cbquery, "sTenNXB", "sMaNXB");
+        }
+
+        private void dgdSach_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) { return; }
+            var row = dgdSach.Rows[e.RowIndex];
+            txtMasach.Text = row.Cells["Mã sách"].Value?.ToString() ?? "";
+            txtTensach.Text = row.Cells["Tên sách"].Value?.ToString() ?? "";
+            txtTheloai.Text = row.Cells["Thể loại"].Value?.ToString() ?? "";
+            txtSoluong.Text = row.Cells["Số lượng"].Value?.ToString() ?? "";
+            txtGia.Text = row.Cells["Giá"].Value?.ToString() ?? "";
+            txtNamxuatban.Text = row.Cells["Năm xuất bản"].Value?.ToString() ?? "";
+            txtTacgia.Text = row.Cells["Tác giả"].Value?.ToString() ?? "";
+            cbbNXB.Text = row.Cells["Nhà xuất bản"].Value?.ToString() ?? "";
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            var parameters = new SqlParameter[]
+            {
+                new SqlParameter("@sMasach",txtMasach.Text),
+                new SqlParameter("@sTensach",txtTensach.Text),
+                new SqlParameter("@sMaNXB",cbbNXB.SelectedValue),
+                new SqlParameter("@sTacgia",txtTacgia.Text),
+                new SqlParameter("@sTheloai",txtTheloai.Text),
+                new SqlParameter("@iGia",Convert.ToInt32(txtGia.Text)),
+                new SqlParameter("@iSL",Convert.ToInt32(txtSoluong.Text)),
+                new SqlParameter("@iNamXB",Convert.ToInt32(txtNamxuatban.Text)),
+            };
+            dBHelper.ExecuteNonQuery("ThemSach", parameters, CommandType.StoredProcedure);
+            LoadData();
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            var parameters = new SqlParameter[]
+            {
+                new SqlParameter("@sMasach",txtMasach.Text),
+                new SqlParameter("@sTensach",txtTensach.Text),
+                new SqlParameter("@sMaNXB",cbbNXB.SelectedValue),
+                new SqlParameter("@sTacgia",txtTacgia.Text),
+                new SqlParameter("@sTheloai",txtTheloai.Text),
+                new SqlParameter("@iGia",Convert.ToInt32(txtGia.Text)),
+                new SqlParameter("@iSL",Convert.ToInt32(txtSoluong.Text)),
+                new SqlParameter("@iNamXB",Convert.ToInt32(txtNamxuatban.Text)),
+            };
+            dBHelper.ExecuteNonQuery("SuaSach", parameters, CommandType.StoredProcedure);
+            LoadData();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            var parameters = new SqlParameter[]
+            {
+                new SqlParameter("@sMasach",txtMasach.Text)
+            };
+            dBHelper.ExecuteNonQuery("XoaSach", parameters, CommandType.StoredProcedure);
+            LoadData();
+            ClearInput();
+        }
+
+        private void btnLammoi_Click(object sender, EventArgs e)
+        {
+            ClearInput();
+            LoadData();
+        }
+
+        private void ClearInput()
+        {
+            txtGia.Text = string.Empty;
+            txtMasach.Text = string.Empty;
+            txtNamxuatban.Text = string.Empty;
+            txtSoluong.Text = string.Empty;
+            txtTacgia.Text = string.Empty;
+            txtTensach.Text = string.Empty;
+            txtTheloai.Text = string.Empty;
+        }
+
+        private void btnTimkiem_Click(object sender, EventArgs e)
+        {
+            var parameters = new SqlParameter[]
+            {
+                new SqlParameter("@TuKhoa",txtTimkiem.Text)
+            };
+            DataTable dt = new DataTable();
+            dt = dBHelper.ExecuteQuery("TimKiemSachTheoTen", parameters, CommandType.StoredProcedure);
+            dBHelper.FillDataGridView(dgdSach, dt);
+        }
+
+        //private void dgdSach_RowValidated(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    if (dgdSach.Rows[e.RowIndex].IsNewRow) return;
+
+        //    DataGridViewRow row = dgdSach.Rows[e.RowIndex];
+        //    string masach = row.Cells["Mã sách"].Value?.ToString() ?? "";
+        //    string tensach = row.Cells["Tên sách"].Value?.ToString() ?? "";
+        //    string theloai = row.Cells["Thể loại"].Value?.ToString() ?? "";
+        //    int soluong = Convert.ToInt32(row.Cells["Số lượng"].Value?.ToString() ?? "");
+        //    int gia = Convert.ToInt32(row.Cells["Giá"].Value?.ToString() ?? "");
+        //    int namxb = Convert.ToInt32(row.Cells["Năm xuất bản"].Value?.ToString() ?? "");
+        //    string tacgia = row.Cells["Tác giả"].Value?.ToString() ?? "";
+        //    string tenNXB = row.Cells["Nhà xuất bản"].Value?.ToString() ?? "";
+
+        //    string sql = "SELECT sMaNXB FROM tblNXB WHERE sTenNXB = @TenNXB";
+        //    SqlParameter[] parameters = {
+        //        new SqlParameter("@TenNXB", tenNXB)
+        //    };
+
+        //    object result = dBHelper.ExecuteScalar(sql, parameters);
+        //    if (result == null)
+        //    {
+        //        MessageBox.Show("Không tìm thấy Nhà xuất bản.");
+        //        return;
+        //    }
+
+        //    string maNXB = result.ToString();
+
+        //    var parameter = new SqlParameter[]
+        //    {
+        //        new SqlParameter("@sMasach",masach),
+        //        new SqlParameter("@sTensach",tensach),
+        //        new SqlParameter("@sMaNXB",maNXB),
+        //        new SqlParameter("@sTacgia",tacgia),
+        //        new SqlParameter("@sTheloai",theloai),
+        //        new SqlParameter("@iGia",gia),
+        //        new SqlParameter("@iSL",soluong),
+        //        new SqlParameter("@iNamXB",namxb),
+        //    };
+        //    if (CheckIfBookExists(masach))
+        //    {
+        //        dBHelper.ExecuteNonQuery("SuaSach", parameter, CommandType.StoredProcedure);
+        //    }
+        //    else
+        //    {
+        //        dBHelper.ExecuteNonQuery("ThemSach", parameter, CommandType.StoredProcedure);
+        //    }
+
+        //}
+
+        private bool CheckIfBookExists(string masach)
+        {
+            string sql = "SELECT COUNT(*) FROM tblSach WHERE sMasach = @sMasach";
+            SqlParameter[] parameters = {
+            new SqlParameter("@sMasach", masach)
+            };
+            object result = dBHelper.ExecuteScalar(sql, parameters);
+            return Convert.ToInt32(result) > 0;
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dgdSach.Rows)
+            {
+                if (row.IsNewRow) continue;
+
+                string masach = row.Cells["Mã sách"].Value?.ToString() ?? "";
+                string tensach = row.Cells["Tên sách"].Value?.ToString() ?? "";
+                string theloai = row.Cells["Thể loại"].Value?.ToString() ?? "";
+                string tacgia = row.Cells["Tác giả"].Value?.ToString() ?? "";
+                string tenNXB = row.Cells["Nhà xuất bản"].Value?.ToString() ?? "";
+
+                // Chuyển đổi số an toàn
+                bool isSoLuongValid = int.TryParse(row.Cells["Số lượng"].Value?.ToString(), out int soluong);
+                bool isGiaValid = int.TryParse(row.Cells["Giá"].Value?.ToString(), out int gia);
+                bool isNamXBValid = int.TryParse(row.Cells["Năm xuất bản"].Value?.ToString(), out int namxb);
+
+                if (!isSoLuongValid || !isGiaValid || !isNamXBValid)
+                {
+                    MessageBox.Show("Dữ liệu số không hợp lệ ở dòng chứa mã sách: " + masach);
+                    continue;
+                }
+
+                // Lấy mã NXB từ tên NXB
+                string sql = "SELECT sMaNXB FROM tblNXB WHERE sTenNXB = @TenNXB";
+                SqlParameter[] parameters = {
+                    new SqlParameter("@TenNXB", tenNXB)
+                };
+
+                object result = dBHelper.ExecuteScalar(sql, parameters);
+                if (result == null)
+                {
+                    MessageBox.Show($"Không tìm thấy Nhà xuất bản: {tenNXB} cho sách: {masach}");
+                    continue;
+                }
+
+                string maNXB = result.ToString();
+
+                var parameter = new SqlParameter[]
+                {
+                    new SqlParameter("@sMasach", masach),
+                    new SqlParameter("@sTensach", tensach),
+                    new SqlParameter("@sMaNXB", maNXB),
+                    new SqlParameter("@sTacgia", tacgia),
+                    new SqlParameter("@sTheloai", theloai),
+                    new SqlParameter("@iGia", gia),
+                    new SqlParameter("@iSL", soluong),
+                    new SqlParameter("@iNamXB", namxb),
+                };
+
+                if (CheckIfBookExists(masach))
+                {
+                    dBHelper.ExecuteNonQuery("SuaSach", parameter, CommandType.StoredProcedure);
+                }
+                else
+                {
+                    dBHelper.ExecuteNonQuery("ThemSach", parameter, CommandType.StoredProcedure);
+                }
+            }
+
+            MessageBox.Show("Đã lưu thành công!");
+            LoadData(); // Nếu có hàm load lại dữ liệu
+        }
+
     }
 }
