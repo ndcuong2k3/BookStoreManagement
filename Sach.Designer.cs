89422e45c2dc3a88d6ee734dfba61ae2cb035d57
ndcuong2k3 <21a100100051@students.hou.edu.vn>
2025-06-10 14:19:34 +0700

    Arrange Form's Component

--- Sach.Designer.cs
+++ Sach.Designer.cs
@@ -46,20 +46,35 @@
             label2 = new Label();
             label1 = new Label();
             dgdSach = new DataGridView();
-            txtTimkiem = new TextBox();
             btnThem = new Button();
             btnSua = new Button();
             btnXoa = new Button();
             btnLammoi = new Button();
-            btnTimkiem = new Button();
             button6 = new Button();
             btnLuu = new Button();
+            checkBox1 = new CheckBox();
+            btnTimkiem = new Button();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            checkBox5 = new CheckBox();
+            checkBox6 = new CheckBox();
+            checkBox8 = new CheckBox();
+            checkBox9 = new CheckBox();
             groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgdSach).BeginInit();
             SuspendLayout();
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(checkBox8);
+            groupBox1.Controls.Add(checkBox9);
+            groupBox1.Controls.Add(checkBox4);
+            groupBox1.Controls.Add(checkBox5);
+            groupBox1.Controls.Add(checkBox6);
+            groupBox1.Controls.Add(checkBox3);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox1);
             groupBox1.Controls.Add(cbbNXB);
             groupBox1.Controls.Add(label4);
             groupBox1.Controls.Add(txtGia);
@@ -76,9 +91,9 @@
             groupBox1.Controls.Add(label3);
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(label1);
-            groupBox1.Location = new Point(12, 19);
+            groupBox1.Location = new Point(34, 19);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(332, 324);
+            groupBox1.Size = new Size(417, 366);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Thông tin sách";
@@ -87,7 +102,7 @@
             // 
             cbbNXB.DropDownStyle = ComboBoxStyle.DropDownList;
             cbbNXB.FormattingEnabled = true;
-            cbbNXB.Location = new Point(128, 275);
+            cbbNXB.Location = new Point(128, 312);
             cbbNXB.Name = "cbbNXB";
             cbbNXB.Size = new Size(198, 28);
             cbbNXB.TabIndex = 1;
@@ -95,7 +110,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(11, 278);
+            label4.Location = new Point(11, 315);
             label4.Name = "label4";
             label4.Size = new Size(97, 20);
             label4.TabIndex = 3;
@@ -103,42 +118,42 @@
             // 
             // txtGia
             // 
-            txtGia.Location = new Point(128, 242);
+            txtGia.Location = new Point(128, 273);
             txtGia.Name = "txtGia";
             txtGia.Size = new Size(198, 27);
             txtGia.TabIndex = 15;
             // 
             // txtTheloai
             // 
-            txtTheloai.Location = new Point(128, 209);
+            txtTheloai.Location = new Point(128, 234);
             txtTheloai.Name = "txtTheloai";
             txtTheloai.Size = new Size(198, 27);
             txtTheloai.TabIndex = 14;
             // 
             // txtTacgia
             // 
-            txtTacgia.Location = new Point(128, 176);
+            txtTacgia.Location = new Point(128, 195);
             txtTacgia.Name = "txtTacgia";
             txtTacgia.Size = new Size(198, 27);
             txtTacgia.TabIndex = 13;
             // 
             // txtNamxuatban
             // 
-            txtNamxuatban.Location = new Point(128, 143);
+            txtNamxuatban.Location = new Point(128, 156);
             txtNamxuatban.Name = "txtNamxuatban";
             txtNamxuatban.Size = new Size(198, 27);
             txtNamxuatban.TabIndex = 12;
             // 
             // txtSoluong
             // 
-            txtSoluong.Location = new Point(128, 110);
+            txtSoluong.Location = new Point(128, 117);
             txtSoluong.Name = "txtSoluong";
             txtSoluong.Size = new Size(198, 27);
             txtSoluong.TabIndex = 11;
             // 
             // txtTensach
             // 
-            txtTensach.Location = new Point(128, 74);
+            txtTensach.Location = new Point(128, 78);
             txtTensach.Name = "txtTensach";
             txtTensach.Size = new Size(198, 27);
             txtTensach.TabIndex = 10;
@@ -153,7 +168,7 @@
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(11, 143);
+            label9.Location = new Point(11, 159);
             label9.Name = "label9";
             label9.Size = new Size(102, 20);
             label9.TabIndex = 8;
@@ -162,7 +177,7 @@
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(13, 247);
+            label7.Location = new Point(11, 276);
             label7.Name = "label7";
             label7.Size = new Size(31, 20);
             label7.TabIndex = 6;
@@ -171,7 +186,7 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(11, 212);
+            label6.Location = new Point(11, 237);
             label6.Name = "label6";
             label6.Size = new Size(62, 20);
             label6.TabIndex = 5;
@@ -180,7 +195,7 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(11, 174);
+            label5.Location = new Point(11, 198);
             label5.Name = "label5";
             label5.Size = new Size(55, 20);
             label5.TabIndex = 4;
@@ -189,7 +204,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(11, 110);
+            label3.Location = new Point(11, 120);
             label3.Name = "label3";
             label3.Size = new Size(97, 20);
             label3.TabIndex = 2;
@@ -198,7 +213,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(11, 74);
+            label2.Location = new Point(11, 78);
             label2.Name = "label2";
             label2.Size = new Size(65, 20);
             label2.TabIndex = 1;
@@ -216,25 +231,18 @@
             // dgdSach
             // 
             dgdSach.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgdSach.Location = new Point(356, 67);
+            dgdSach.Location = new Point(465, 32);
             dgdSach.Name = "dgdSach";
             dgdSach.RowHeadersWidth = 51;
-            dgdSach.Size = new Size(696, 323);
+            dgdSach.Size = new Size(587, 353);
             dgdSach.TabIndex = 1;
             dgdSach.CellClick += dgdSach_CellClick;
             // 
-            // txtTimkiem
-            // 
-            txtTimkiem.Location = new Point(356, 34);
-            txtTimkiem.Name = "txtTimkiem";
-            txtTimkiem.Size = new Size(582, 27);
-            txtTimkiem.TabIndex = 17;
-            // 
             // btnThem
             // 
-            btnThem.Location = new Point(12, 361);
+            btnThem.Location = new Point(34, 395);
             btnThem.Name = "btnThem";
-            btnThem.Size = new Size(94, 29);
+            btnThem.Size = new Size(100, 36);
             btnThem.TabIndex = 18;
             btnThem.Text = "Thêm";
             btnThem.UseVisualStyleBackColor = true;
@@ -242,9 +250,9 @@
             // 
             // btnSua
             // 
-            btnSua.Location = new Point(129, 361);
+            btnSua.Location = new Point(139, 395);
             btnSua.Name = "btnSua";
-            btnSua.Size = new Size(94, 29);
+            btnSua.Size = new Size(100, 36);
             btnSua.TabIndex = 19;
             btnSua.Text = "Sửa";
             btnSua.UseVisualStyleBackColor = true;
@@ -252,9 +260,9 @@
             // 
             // btnXoa
             // 
-            btnXoa.Location = new Point(244, 361);
+            btnXoa.Location = new Point(245, 395);
             btnXoa.Name = "btnXoa";
-            btnXoa.Size = new Size(94, 29);
+            btnXoa.Size = new Size(100, 36);
             btnXoa.TabIndex = 20;
             btnXoa.Text = "Xóa";
             btnXoa.UseVisualStyleBackColor = true;
@@ -262,29 +270,19 @@
             // 
             // btnLammoi
             // 
-            btnLammoi.Location = new Point(12, 396);
+            btnLammoi.Location = new Point(611, 395);
             btnLammoi.Name = "btnLammoi";
-            btnLammoi.Size = new Size(326, 29);
+            btnLammoi.Size = new Size(143, 36);
             btnLammoi.TabIndex = 21;
             btnLammoi.Text = "Làm mới";
             btnLammoi.UseVisualStyleBackColor = true;
             btnLammoi.Click += btnLammoi_Click;
             // 
-            // btnTimkiem
-            // 
-            btnTimkiem.Location = new Point(958, 32);
-            btnTimkiem.Name = "btnTimkiem";
-            btnTimkiem.Size = new Size(94, 29);
-            btnTimkiem.TabIndex = 22;
-            btnTimkiem.Text = "Tìm kiếm";
-            btnTimkiem.UseVisualStyleBackColor = true;
-            btnTimkiem.Click += btnTimkiem_Click;
-            // 
             // button6
             // 
-            button6.Location = new Point(909, 396);
+            button6.Location = new Point(909, 395);
             button6.Name = "button6";
-            button6.Size = new Size(143, 29);
+            button6.Size = new Size(143, 36);
             button6.TabIndex = 23;
             button6.Text = "Về Trang chủ";
             button6.UseVisualStyleBackColor = true;
@@ -292,27 +290,107 @@
             // 
             // btnLuu
             // 
-            btnLuu.Location = new Point(760, 396);
+            btnLuu.Location = new Point(760, 395);
             btnLuu.Name = "btnLuu";
-            btnLuu.Size = new Size(143, 29);
+            btnLuu.Size = new Size(143, 36);
             btnLuu.TabIndex = 24;
             btnLuu.Text = "Lưu";
             btnLuu.UseVisualStyleBackColor = true;
             btnLuu.Click += btnLuu_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(359, 45);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(18, 17);
+            checkBox1.TabIndex = 16;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnTimkiem
+            // 
+            btnTimkiem.Location = new Point(351, 395);
+            btnTimkiem.Name = "btnTimkiem";
+            btnTimkiem.Size = new Size(100, 36);
+            btnTimkiem.TabIndex = 25;
+            btnTimkiem.Text = "Tìm kiếm";
+            btnTimkiem.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(359, 84);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(18, 17);
+            checkBox2.TabIndex = 17;
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(359, 120);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(18, 17);
+            checkBox3.TabIndex = 18;
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(359, 237);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(18, 17);
+            checkBox4.TabIndex = 21;
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(359, 201);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(18, 17);
+            checkBox5.TabIndex = 20;
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            checkBox6.AutoSize = true;
+            checkBox6.Location = new Point(359, 162);
+            checkBox6.Name = "checkBox6";
+            checkBox6.Size = new Size(18, 17);
+            checkBox6.TabIndex = 19;
+            checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            checkBox8.AutoSize = true;
+            checkBox8.Location = new Point(359, 315);
+            checkBox8.Name = "checkBox8";
+            checkBox8.Size = new Size(18, 17);
+            checkBox8.TabIndex = 23;
+            checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            checkBox9.AutoSize = true;
+            checkBox9.Location = new Point(359, 276);
+            checkBox9.Name = "checkBox9";
+            checkBox9.Size = new Size(18, 17);
+            checkBox9.TabIndex = 22;
+            checkBox9.UseVisualStyleBackColor = true;
             // 
             // Sach
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1082, 453);
+            Controls.Add(btnTimkiem);
             Controls.Add(btnLuu);
             Controls.Add(button6);
-            Controls.Add(btnTimkiem);
             Controls.Add(btnLammoi);
             Controls.Add(btnXoa);
             Controls.Add(btnSua);
             Controls.Add(btnThem);
-            Controls.Add(txtTimkiem);
             Controls.Add(dgdSach);
             Controls.Add(groupBox1);
             Name = "Sach";
@@ -321,7 +399,6 @@
             groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dgdSach).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -344,13 +421,20 @@
         private TextBox txtMasach;
         private Label label9;
         private DataGridView dgdSach;
-        private TextBox txtTimkiem;
         private Button btnThem;
         private Button btnSua;
         private Button btnXoa;
         private Button btnLammoi;
-        private Button btnTimkiem;
         private Button button6;
         private Button btnLuu;
+        private CheckBox checkBox1;
+        private Button btnTimkiem;
+        private CheckBox checkBox8;
+        private CheckBox checkBox9;
+        private CheckBox checkBox4;
+        private CheckBox checkBox5;
+        private CheckBox checkBox6;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
     }
 }
